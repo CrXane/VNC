20dd4328ab0cfe74b952dd8d8a3f506fa7d4accb
CrXane <25283652+CrXane@users.noreply.github.com>
2020-04-15 19:03:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 
 sudo add-apt-repository ppa:gnome3-team/gnome3
 sudo apt-get update
-sudo apt-get install --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal tightvncserver
-sudo apt-get install epiphany-browser
+sudo apt-get install --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal tightvncserver -y
+sudo apt-get install epiphany-browser -y
+sudo apt-get install htop gedit file-roller -y
+sudo apt-get install wine -y
+sudo apt-get install filezilla -y
+sudo apt-get install xfce4-terminal -y
+sudo apt-get install synaptic -y
 
 echo "set a password and execute ./install2.sh"
 vncserver
